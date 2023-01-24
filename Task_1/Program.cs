2af8f4f4056a0df2521fbe0371608cac97cd0a44
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-24 20:09:44 +0300

    Task_1 End

---
+++ Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+
+int n = int.Parse(Console.ReadLine());
+int stepen = 3;
+
+for (int i = 0; i < n; i++)
+
+    {
+        Console.WriteLine(Math.Pow(i, stepen));
+    }

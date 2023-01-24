fbd3f01403594513117b0eaa403440c25efa95d2
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-25 00:40:05 +0600

    Task_3 End

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+int a = int.Parse(Console.ReadLine()); //число
+int b = int.Parse(Console.ReadLine()); //степнь
+int stepen = 1;
+
+for (int i =1; i<b+1;i++)
+{
+stepen = stepen * a;
+}
+
+Console.WriteLine(stepen);

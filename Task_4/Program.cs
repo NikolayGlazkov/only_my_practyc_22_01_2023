73ed84fc0cfd3fe160283887074f07eeda8dc2f1
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-25 11:06:24 +0300

    Task_4 End

---
+++ Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Подсчитать сумму цифр в числе
+
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+while(a > 0)
+{
+    int LastDigit = a %10;
+    sum += LastDigit;
+    a = a/10;
+}
+
+Console.WriteLine(sum);

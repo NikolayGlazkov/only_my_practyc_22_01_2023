17110265b3e63b3c28d92f79229d3863f038f915
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-25 11:13:31 +0300

    wrong commite

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i < a+1; i++)
+{
+    sum = i+sum;
+}
+Console.WriteLine(sum);
---
+++ Task_stepik/Program.cs
@@ -0,0 +1,31 @@
+﻿int a = int.Parse(Console.ReadLine());//первое число
+string input = Console.ReadLine();
+char c = input[0];
+int b = int.Parse(Console.ReadLine());//вторе число
+
+int result = 0;
+int result1 = 0;
+
+while(result > 0)
+{
+switch (c)
+    {
+        case '+':
+            result1 = a + b;
+        break;
+
+        case '-':
+            result = a - b;
+        break;
+
+        case '*':
+            result = a * b;
+        break;
+
+         case '/':
+            result = a / b;
+        break;
+
+    }
+    Console.WriteLine(result);
+}

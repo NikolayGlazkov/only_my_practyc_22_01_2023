0d5e9f35a0c92bb1b7df9be87e14e2cf2307747c
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-25 21:13:37 +0300

    Task_6 End

---
+++ Task_6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+int num = int.Parse(Console.ReadLine());
+int stepen = 3;
+for (int i = 1; i < num;i++)
+{
+    int a = (int)Math.Pow(i,stepen);
+    if (a%2 == 0)
+    Console.WriteLine(a);
+}

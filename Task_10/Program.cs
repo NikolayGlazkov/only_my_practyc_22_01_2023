e5d785178c8dfe15394f24ff55b83b0ce9df94f1
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-26 10:42:28 +0300

    Task_10 END

---
+++ Task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] array = new int[123]; // создание массива длинной 123 числа
+int Count = 0; //счетчик
+
+for(int i = 0; i < array.Length; i++) 
+{
+    array[i] = i;
+    if (array[i]>10 && array[i]<99)
+    {
+    Count = Count + 1;
+    }
+}
+Console.WriteLine(Count);

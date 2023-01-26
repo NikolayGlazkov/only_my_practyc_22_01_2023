be5c0f7ccbb6293290c44743df59587da638f47b
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-26 13:40:55 +0300

    Task_11 End

---
+++ Task_11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] array = new int[123]; // создание массива длинной 123 числа
+int Count = 0; //счетчик
+
+for(int i = 0; i < array.Length; i++) 
+{
+    array[i] = i;
+    if ( array[i]%2 !=0)
+    {
+    Count = Count + 1;
+    }
+}
+Console.WriteLine(Count);

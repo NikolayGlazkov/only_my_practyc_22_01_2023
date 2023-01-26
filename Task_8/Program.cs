3cf913772a8ab00bba39623af94c9d0584bbd8f0
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-26 09:24:06 +0300

    Task_8 END

---
+++ Task_8/Program.cs
@@ -0,0 +1,31 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+int a = int.Parse(Console.ReadLine()); //ввод числа которое мы ищем
+int[] array = new int[10]; // создание массива длинной 10 чисел
+Random random = new Random(); //рандом
+
+
+for(int i = 0; i < array.Length; i++) //заполняет цикл случайными числами, самый сложный момент для понимания мною
+{
+    array[i] = random.Next(0,10);
+}
+
+foreach (int num in array) //Каждая итерация цикла присваивает значение текущего элемента массива переменной num и выводит его
+{
+    Console.Write(num + " ");
+}
+
+bool isFound = false; //Булевая переменная 
+for(int i = 0; i < array.Length; i++)
+{
+if (array[i] == a)
+
+{
+    Console.WriteLine("В массиве есть число числоT:"+a);
+    isFound = true;
+    break;
+}
+}
+if(!isFound) 
+{
+    Console.WriteLine("В массиве нет числа:"+a);
+}

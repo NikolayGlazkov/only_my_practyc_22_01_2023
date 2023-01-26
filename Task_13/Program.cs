a1abbf484ad8a00322cf606ce777b0184d32c28f
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-26 18:26:17 +0300

    Task_13 End

---
+++ Task_13/Program.cs
@@ -0,0 +1,21 @@
+﻿// В Указанном массиве вещественных чисел найдите  максимальное и минимальное значение элементом
+
+
+int [] array = {1, 2, 3, 4, 5, 9, 4, -1, -2, -5};
+int max = array[0];
+int min = array[0];
+
+int otvet = 0;
+
+for (int i = 0; i < array.Length ; i++)
+{
+    if ( array[i] > max)
+    max = array[i];
+
+    if( array[i]< min)
+    min = array[i];
+}
+
+otvet = (max - min);
+
+Console.WriteLine("разница между большим и ментщим равна: "+otvet);
